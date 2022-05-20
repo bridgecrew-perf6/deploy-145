a79426b0b4f9a15e523cd6441aecc2be974ef3d9
vengammaa <104969044+vengammaa@users.noreply.github.com>
2022-05-20 16:07:12 +0530

    Delete apitest directory

--- apitest/src/apitest/App.java
+++
@@ -1,8 +0,0 @@
-package apitest;
-
-public class App {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Helloworld");
-	}
-}
--- apitest/src/com/api/test/Helloworld.java
+++
@@ -1,8 +0,0 @@
-package com.api.test;
-
-public class Helloworld {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
- System.out.println("Helloworld");
-	}
-}
